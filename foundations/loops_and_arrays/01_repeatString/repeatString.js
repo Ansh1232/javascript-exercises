562b14e92fb8fc6b78c6f4239f169d53db4356e3
Ubuntu <ubuntu@LAPTOP-TP2HIRJM.localdomain>
2026-09-12 12:19:25 +0000

    repeatstringprogram in js

--- foundations/loops_and_arrays/01_repeatString/repeatString.js
+++ foundations/loops_and_arrays/01_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(strr,n) {
+    
+let retStrings="";
+for(i=0;i<n;i++){
+    retStrings+=" "+strr;
+}
+return retStrings;
 };
+console.log(repeatString("Hello",3))
 
 // Do not edit below this line
 module.exports = repeatString;
